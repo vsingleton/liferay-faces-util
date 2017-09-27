cb07d1eb990522d61411fe4ee01b9b6e076128e7
vsingleton <vsingleton@gmail.com>
2017-09-27 10:26:12 -0400

    FACES-2532 - Incorporate Semantic Versioning detection via the baseline goal of the bnd-baseline-maven-plugin

---
+++ src/main/java/com/liferay/faces/util/el/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.liferay.faces.util.el;
+
+import org.osgi.annotation.versioning.Version;
---
+++ src/main/java/com/liferay/faces/util/render/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.liferay.faces.util.render;
+
+import org.osgi.annotation.versioning.Version;
